d3dce1c6861f62b16bf4f972618d2762cf5cdc4d
Ojha <Abhishek.Ojha@philips.com>
2023-08-22 00:10:58 +0530

    Basic folder structure

---
+++ src/screens/Landing.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Landing() {
+  return (
+    <>
+      <div className="section" id="section1">
+        <h1 id="landingtext">What we do today will affect someone's life tomorrow</h1>
+      </div>
+    </>
+  );
+}
