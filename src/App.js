89055fd759aac29596b6c873dfbc8057ba3bd911
Krishna Karvariya <satyamkarvariya2510@gmail.com>
2023-08-22 23:20:44 +0530

    Added the basic navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Landing from './screens/Landing';
 import Stats from "./screens/Stats"
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <NavBar></NavBar>
     <Landing></Landing>
     <Stats></Stats>
     </div>
